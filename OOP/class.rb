8bd651be30f2d8a81267eb929f5031e149f2f739
NgnDinhThanh <thanh.nd200592@sis.hust.edu.vn>
2025-09-12 15:56:23 +0700

    create class and object

---
+++ OOP/class.rb
@@ -0,0 +1,52 @@
+class Box
+    @@count = 0
+    # constructor method
+    def initialize w, h
+        @width, @height =w, h
+        @@count += 1
+    end
+
+    def self.printCount
+        puts "Box count is : #@@count"
+    end
+
+    # accessor methods
+    def getWidth
+        @width
+    end
+
+    def getHeight
+        @height
+    end
+
+    # setter methods
+    def setWidth value
+        @width = value
+    end
+    def setHeight value
+        @height = value
+    end
+
+    def getArea
+        @width * @height
+    end
+end
+
+# create an object
+box = Box.new 10, 20
+box2 = Box.new 20, 50
+
+# use setter methods
+box.setWidth 30
+box.setHeight 50
+
+# use accessor methods
+x = box.getWidth
+y = box.getHeight
+area = box.getArea
+
+puts "Width of the box is : #{x}"
+puts "Height of the box is : #{y}"
+puts "Area of the box is : #{area}"
+
+Box.printCount
