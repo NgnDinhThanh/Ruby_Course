ffb3e33cbb6cb24aad07f4ca1674de98c2ba4d12
NgnDinhThanh <thanh.nd200592@sis.hust.edu.vn>
2025-09-12 10:01:58 +0700

    while loop

---
+++ Iterator_and_Loops/while_loop.rb
@@ -0,0 +1,13 @@
+puts "1. While do"
+i = 0
+while i < 10 do
+    puts("Inside the loop i = #{i}" )
+    i +=1
+end
+
+puts "2. Begin While"
+i = 0
+begin
+    puts("Inside the loop i = #{i}" )
+    i +=1
+end while i < 10
