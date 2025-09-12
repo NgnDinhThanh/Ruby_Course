fdce5f50617f53d7f7513c6263b0a75096ba6432
NgnDinhThanh <thanh.nd200592@sis.hust.edu.vn>
2025-09-12 07:57:54 +0700

    first commit

---
+++ interator.rb
@@ -0,0 +1,10 @@
+puts "====== Iterator ============="
+5.times {print "Hello World!!!\n"}
+puts "=============================="
+puts ""
+
+# Operator
+puts "8 + 9 = #{8 + 9}"
+puts "8 - 9 = #{8 - 9}"
+puts "=============================="
+puts ""
